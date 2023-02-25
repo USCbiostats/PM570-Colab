77a9a818d96a7a0dd6991ea620d4db5d2d272d57
Nicholas Mancuso <nick.mancuso@gmail.com>
2023-02-24 16:46:49 -0800

    added gwas ols

--- util/gwas.py
+++ util/gwas.py
@@ -1,4 +1,5 @@
 import jax
+import jax.lax as lax
 import jax.numpy as jnp
 import jax.scipy as jsp
 import pandas as pd
@@ -6,36 +7,48 @@ import pandas as pd
 from scipy import stats
 
 
+@jax.jit
+def _trait_scan_ols(X, y):
+    n, p = X.shape
+    covar = jnp.ones((n,))
+    results = lax.scan(ols, y, X.T)
+    return results
+
+
+def trait_scan_ols(X, y):
+    results = _trait_scan_ols(X, y)
+    return pd.DataFrame(
+        {
+            "beta": results.T[0],
+            "se": results.T[1],
+            "tscore": results.T[2],
+            "pvalue": results.T[3],
+        }
+    )
+
+
 # OLS with variables (x, covar) and y
-def ols(x, y, covar=None):
+def ols(X, y):
     n = len(y)
-    if covar is not None:
-        X = jnp.hstack((x[:, jnp.newaxis], covar))
-        X = (X - jnp.mean(X, axis=0)) / jnp.std(X, axis=0)
+    covar = jnp.ones((n,))
+    X = jnp.hstack((x[:, jnp.newaxis], covar))
 
-        q_matrix, r_matrix = jnp.linalg.qr(X, mode="reduced")
-        qty = q_matrix.T @ y
-        beta_hat = jsp.linalg.solve_triangular(r_matrix, qty)
-        df = q_matrix.shape[0] - q_matrix.shape[1]
-        residual = y - q_matrix @ qty
-        rss = jnp.sum(residual**2, axis=0)
-        sigma = jnp.sqrt(jnp.sum(residual**2) / df)
-        se = (
-            jnp.sqrt(
-                jnp.diag(
-                    jsp.linalg.cho_solve((r_matrix, False), jnp.eye(r_matrix.shape[0]))
-                )
+    q_matrix, r_matrix = jnp.linalg.qr(X, mode="reduced")
+    qty = q_matrix.T @ y
+    beta_hat = jsp.linalg.solve_triangular(r_matrix, qty)
+    df = q_matrix.shape[0] - q_matrix.shape[1]
+    residual = y - q_matrix @ qty
+    rss = jnp.sum(residual**2, axis=0)
+    sigma = jnp.sqrt(jnp.sum(residual**2) / df)
+    se = (
+        jnp.sqrt(
+            jnp.diag(
+                jsp.linalg.cho_solve((r_matrix, False), jnp.eye(r_matrix.shape[0]))
             )
-            * sigma
         )
-        t_scores = beta_hat / se
-        p_value = jnp.array(2 * stats.t.sf(abs(t_scores), df=df))
-    else:
-        x = (x - jnp.mean(x)) / jnp.std(x)
-        beta_hat = (x.T @ y) / n  # x.T @ x ~ n
-        s2 = jnp.mean((y - x * beta_hat) ** 2)
-        se = jnp.sqrt(s2 / n)
-        z = beta_hat / se
-        pvalue = 2 * stats.norm.cdf(-jnp.abs(z))
+        * sigma
+    )
+    t_scores = beta_hat / se
+    p_value = jnp.array(2 * stats.t.sf(abs(t_scores), df=df))
 
-    return beta_hat, se, t_scores, pvalue
+    return jnp.array([beta_hat, se, t_scores, pvalue]).T[0]
