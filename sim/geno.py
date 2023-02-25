afb0fa9995d795888a907f1e96ee7845065b3697
Nicholas Mancuso <nick.mancuso@gmail.com>
2023-02-24 16:03:35 -0800

    updated to use plink simnulation

--- sim/geno.py
+++ sim/geno.py
@@ -1,7 +1,9 @@
 import jax
 import jax.numpy as jnp
+import jax.numpy.linalg as jnla
 import jax.random as rdm
 
+import pandas_plink as pdp
 
 # naive simulation of genotype with no LD structure
 def naive_sim_genotype(n_samples: int, p_snps: int, rng_key):
@@ -12,3 +14,33 @@ def naive_sim_genotype(n_samples: int, p_snps: int, rng_key):
   X = h1 + h2
 
   return X
+
+def sim_geno_from_plink(prefix: str, n: int, rng_key, ld_ridge: float = 0.1):
+
+  # return cholesky L and ldscs
+  bim, fam, G = pdp.read_plink(prefix, verbose=False)
+  G = G.T
+
+  # estimate LD for population from PLINK data
+  n, p = [float(x) for x in G.shape]
+  p_int = int(p)
+  mafs = jnp.mean(G, axis=0) / 2
+  G = (G - jnp.mean(G, axis=0)) / jnp.std(G, axis=0) 
+
+  # regularize so that LD is PSD
+  LD = jnp.dot(G.T, G) / n + jnp.eye(p_int) * ld_ridge
+
+  # re-adjust to get proper correlation matrix
+  LD = LD / (1 + ld_ridge)
+
+  # compute cholesky decomp for faster sampling/simulation
+  L = jnla.cholesky(LD, lower=True)
+
+  p, p = L.shape
+
+  Z = (L @ rdm.normal(size=(n,p)).T).T
+  Z -= jnp.mean(Z, axis=0)
+  Z /= jnp.std(Z, axis=0)
+
+  return Z
+
